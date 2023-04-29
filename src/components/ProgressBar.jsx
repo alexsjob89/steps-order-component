a9f5a8654e7ed5f0b201ca03ae63169d9a725359
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-04-29 23:37:24 +0100

    one last change

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function ProgressBar({progress}) {
   return (
     <div>
-<LinearProgress variant='determinate' value={progress} style={{backgroundColor: "red", height: "14px", width: "20rem"}}/>
+<LinearProgress variant='determinate' value={progress} style={{backgroundColor: "red", height: "14px", width: "20rem",borderRadius: "10px"}}/>
     </div>
   )
 }
